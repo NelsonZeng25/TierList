06c9c647be2d27beb402dcc3f66c50923d6b65b4
Nelson Zeng <nelson.zeng25@gmail.com>
2020-04-06 02:04:58 -0400

    Made the edit details dialog better

--- tierlist-client/src/components/profile/EditDetails.js
+++ tierlist-client/src/components/profile/EditDetails.js
@@ -14,12 +14,42 @@ import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import InputAdornment from '@material-ui/core/InputAdornment';
 
+// Icons
 import EditIcon from '@material-ui/icons/Edit';
+import PersonIcon from '@material-ui/icons/Person';
+import LocationIcon from '@material-ui/icons/LocationOn';
+import LinkIcon from '@material-ui/icons/Link';
+import DescriptionIcon from '@material-ui/icons/Description';
 
 const styles = theme => ({
     ...theme.spreadThis,
-    button: {}
+    button: {},
+    dialog: {
+        "& .MuiPaper-root": {
+            backgroundColor: theme.palette.primary.main,
+        }
+    },
+    textField: {
+        width: '100%',
+        marginTop: '20px',
+        // '& .MuiInputBase-input': {
+        //     color: '#fff', // Text color
+        // },
+        "& .MuiInputLabel-root": {
+            color: theme.palette.text.primaryStrong,
+        },
+        '& .MuiInput-underline:before': {
+            borderBottomColor: theme.palette.text.primary, // Semi-transparent underline
+        },
+        '& .MuiInput-underline:hover:before': {
+            borderBottomColor: theme.palette.text.primaryStrong, // Solid underline on hover
+        },
+        '& .MuiInput-underline:after': {
+            borderBottomColor: theme.palette.text.primaryStrong, // Solid underline on focus
+        },
+    },
 });
 
 class EditDetails extends Component {
@@ -73,24 +103,56 @@ class EditDetails extends Component {
         return (
             <Fragment>
                 <MyButton tip="Edit details" placement="bottom" onClick={this.handleOpen} btnClassName={classes.button}>
-                      <EditIcon color="secondary"/>
+                    <EditIcon color="secondary"/>
                 </MyButton>
-                <Dialog open={this.state.open} onClose={this.handleClose} fullWidth maxWidth="sm">
+                <Dialog className={classes.dialog} open={this.state.open} onClose={this.handleClose} fullWidth maxWidth="sm">
                     <DialogTitle>Edit your details</DialogTitle>
                     <DialogContent>
                         <form>
                             <TextField name="userName" type="text" label="Username" 
                                 placeholder={this.state.userName} className={classes.textField}
-                                value={this.state.userName} onChange={this.handleChange} fullWidth></TextField>
-                            <TextField name="bio" type="text" label="Bio" multiline rows="3" 
+                                value={this.state.userName} onChange={this.handleChange} fullWidth
+                                InputProps={{
+                                    startAdornment: (
+                                      <InputAdornment position="start">
+                                        <PersonIcon color="secondary" />
+                                      </InputAdornment>
+                                    ),
+                                  }}
+                            ></TextField>
+                            <TextField name="bio" type="text" label="Bio" multiline 
                                 placeholder="A short bio about yourself" className={classes.textField}
-                                value={this.state.bio} onChange={this.handleChange} fullWidth></TextField>
+                                value={this.state.bio} onChange={this.handleChange} fullWidth
+                                InputProps={{
+                                    startAdornment: (
+                                      <InputAdornment position="start">
+                                        <DescriptionIcon color="secondary"/>
+                                      </InputAdornment>
+                                    ),
+                                  }}
+                            ></TextField>
                             <TextField name="website" type="text" label="Website" 
                                 placeholder="Your personal/professional website" className={classes.textField}
-                                value={this.state.website} onChange={this.handleChange} fullWidth></TextField>
+                                value={this.state.website} onChange={this.handleChange} fullWidth
+                                InputProps={{
+                                    startAdornment: (
+                                      <InputAdornment position="start">
+                                        <LinkIcon color="secondary"/>
+                                      </InputAdornment>
+                                    ),
+                                  }}
+                            ></TextField>
                             <TextField name="location" type="text" label="Location" 
                                 placeholder="Your location" className={classes.textField}
-                                value={this.state.location} onChange={this.handleChange} fullWidth></TextField>
+                                value={this.state.location} onChange={this.handleChange} fullWidth
+                                InputProps={{
+                                    startAdornment: (
+                                      <InputAdornment position="start">
+                                        <LocationIcon color="secondary"/>
+                                      </InputAdornment>
+                                    ),
+                                  }}
+                            ></TextField>
                             
                         </form>
                     </DialogContent>
--- tierlist-functions/functions/util/validators.js
+++ tierlist-functions/functions/util/validators.js
@@ -44,15 +44,17 @@ exports.validateLoginData = (data) => {
 exports.reduceUserDetails = (data) => {
   let userDetails = {};
 
-  if (!isEmpty(data.userName.trim())) userDetails.userName = data.userName;
-  if (!isEmpty(data.bio.trim())) userDetails.bio = data.bio;
-  if (!isEmpty(data.website.trim())) {
-    // https://website.com
-    if (data.website.trim().substring(0, 4) !== 'http') {
-      userDetails.website = `https://${data.website.trim()}`;
-    } else userDetails.website = data.website;
+  if (!isEmpty(data.userName.trim())) userDetails.userName = data.userName.trim();
+
+  userDetails.bio = data.bio.trim();
+
+  if (!isEmpty(data.website.trim()) && data.website.trim().substring(0, 4) !== 'http') {
+    userDetails.website = `https://${data.website.trim()}`;
+  } else {
+    userDetails.website = data.website.trim();
   }
-  if (!isEmpty(data.location.trim())) userDetails.location = data.location;
+
+  userDetails.location = data.location.trim();
 
   return userDetails;
 }
