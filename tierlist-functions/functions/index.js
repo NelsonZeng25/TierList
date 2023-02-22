d17462a7dc08d4a4738294039e9c5fa778c7b31a
NelsonZeng25 <nelson.zeng25@gmail.com>
2023-02-22 00:59:41 -0500

    Switched to dev env

--- tierlist-functions/functions/index.js
+++ tierlist-functions/functions/index.js
@@ -5,13 +5,16 @@ const app = express();
 
 // https://stackoverflow.com/questions/46337471/how-to-allow-cors-in-react-js
 app.use(function(req, res, next) {
-    // var allowedOrigins = ['http://localhost:3000', 'https://tierlist-57d59.web.app', 'https://tierlist-57d59.firebaseapp.com'];
-    var allowedOrigins = ['https://tierlist-57d59.web.app', 'https://tierlist-57d59.firebaseapp.com'];
-    var origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-    // res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
+    // Dev env
+    res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Prod env
+    // var allowedOrigins = ['https://tierlist-57d59.web.app', 'https://tierlist-57d59.firebaseapp.com'];
+    // var origin = req.headers.origin;
+    // if (allowedOrigins.includes(origin)) {
+    //     res.setHeader('Access-Control-Allow-Origin', origin);
+    // }
+    
     res.set('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
     res.set('Access-Control-Allow-Headers', 'Authorization, Content-Type');
     res.set('Access-Control-Allow-Credentials', 'true');
